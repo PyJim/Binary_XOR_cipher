76aba6aa70b06ae30aa5bb63a72139fcd0b8d589
Jimmy Kodwo Essel <jimmy.essel@kibo.school>
2022-12-08 03:12:44 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# XOR_Cipher
+# XOR Cipher
 XOR cipher encryption algorithm with python
+
+# Binary XOR Cipher Algorithm
+
+This is a simple implementation of the binary XOR cipher, a type of symmetric-key encryption algorithm that operates on binary data. It can be used to encrypt and decrypt messages using a shared key.
+
+## Usage
+
+To use the binary XOR cipher, run the main.py file and enter the value to be encrypted or decrypted as input followed by the key to be used.
+
+For an XOR operation, encryption and decryption use the same function, and hence no need to worry about either encrypting or decrypting as the results will be the same.
+
+The process is smooth when the key and value have the same number of bits. However, necessary adjustments are made to ensure proper encryption if otherwise. 
+
+## Security
+
+The security of the XOR cipher depends on the strength of the key. A strong key that is difficult to guess or brute-force will make it more difficult for an attacker to decrypt the message. 
+It is important to choose a key that is long and random and to keep the key secret.
+
+Additionally, the XOR cipher is vulnerable to known-plaintext attacks, where an attacker has access to both the encrypted and unencrypted versions of the same message. 
+In this case, the attacker can easily determine the key by performing the XOR operation on the known plaintext and the corresponding ciphertext. 
+Therefore, it is important to use the XOR cipher only for short messages or in situations where known-plaintext attacks are not a concern.
+
+
+
+
+
+
+
---
+++ helper.py
@@ -0,0 +1,67 @@
+def intro():
+    """_summary_
+       Allow user inputs of the binary to be encrypted and the key to be used also in binary
+    """
+    global item, key
+    item = input("Enter binary to enrypt: ")
+    key = input("Enter key of encryption: ")
+
+
+def verify():
+    """_summary_
+        Checks if the entries from the intro() function are indeed binary
+    Returns:
+        _bool_: _Returns True or False depending on the results_
+    """
+    global key, item
+    binary = ["0","1"]
+    result1 = all(bit in binary for bit in list(item))
+    result2 = all(bit in binary for bit in list(key))
+    return result1 and result2
+
+
+def modify_key():
+    """_summary_
+       modifies the length of the key to ensure proper encryption
+    """
+    global key, item
+    if len(key)<len(item):
+        key = int(len(item)/len(key))*key
+        if len(key)<len(item):
+            key+=key[0:len(item)%len(key)]
+    elif len(key)>len(item):
+        key = key[0:len(item)]
+
+def encrypt():
+    """_summary_
+        encrypts the value with the key and returns the encrypted value in binary format
+    Returns:
+        _string_: _encrypted value (cipher text)_
+    """
+    global encrypted, key, item
+    encrypted = ""
+    for bit in range(len(item)):
+        if item[bit]==key[bit]:
+            encrypted+="0"
+        else:
+            encrypted+="1"
+    return encrypted
+
+def error():
+    """_summary_
+        returns an error message for invalid inputs
+    Returns:
+        _string_: _custom error message_
+    """
+    return "Invalid key or value"
+
+def start():
+    """_summary_
+       Starts the program and calls the helper functions at appropriate times
+    """
+    intro()
+    if verify():
+        modify_key()
+        print(encrypt())
+    else:
+        print(error())
